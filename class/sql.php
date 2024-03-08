9199329dc5d06a8c8a4d921f313e8a9b77447507
Inaldo Oliveira <ibktecnologia@gmail.com>
2024-03-08 18:28:31 -0300

    iniciamos o projeto

---
+++ class/sql.php
@@ -0,0 +1,63 @@
+<?php 
+
+class Sql extends PDO {
+
+	private $conn;
+
+	public function __construct(){
+
+		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost","ibk","sql308197");
+
+
+	}
+
+	private function setParams($statement, $parameters = array()){
+
+		foreach ($parameters as $key => $value) {
+			
+			$this->setParam($statement, $key, $value);
+
+		}
+
+	}
+
+	private function setParam($statement, $key, $value){
+
+		$statement->bindParam($key, $value);
+
+	}
+
+//	public function query($rawQuery, $params = array()){
+
+//		$stmt = $this->conn->prepare($rawQuery);
+
+//		$this->setParams($stmt, $params);
+
+//		$stmt->execute();
+
+//		return $stmt;
+
+//	}
+
+    public function execQuery($rawQuery, $params = array())
+    {
+        $stmt = $this->conn->prepare($rawQuery);
+ 
+        $this->setParams($stmt, $params);
+ 
+        $stmt->execute();
+        return $stmt;
+    }
+
+	public function select($rawQuery, $params = array()):array
+	{
+
+		$stmt = $this->execQuery($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	}
+
+}
+
+ ?>
