c85f0a60361eb1462c38a9e2bd747e8af0d3dffc
Inaldo Oliveira <ibktecnologia@gmail.com>
2024-03-12 18:53:01 -0300

    Inserir, Alterar e Deletar

--- class/usuario1.php
+++ class/usuario1.php
@@ -46,8 +46,12 @@ class usuario{
 
           if (count($results) > 0) {
 
-            $this->setData($results[0]);
+    	     $row = $results[0];
 
+    	     $this->setIdusuario($row['idusuario']);
+		     $this->setDeslogin($row['deslogin']);    	
+		     $this->setDessenha($row['dessenha']);		
+		     $this->setDtcadastro(new DateTime($row['dtcadastro']));		
            }
 
     }
@@ -80,7 +84,14 @@ class usuario{
 
 		if (count($results) > 0) {
 
-           $this->setData($results[0]);
+            $row = $results[0];
+
+            $this->setData($results[0]);
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);    	
+            $this->setDessenha($row['dessenha']);		
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));		
 
 		} else {
 
@@ -112,41 +123,6 @@ class usuario{
         }
 
     }
-
-    public function update($login, $password){
-
-		$this->setDeslogin($login);
-		$this->setDessenha($password);
-
-		$sql = new Sql();
-
-        $sql->execQuery("UPDATE tb_usuarios SET deslogin= :LOGIN, dessenha= :PASSWORD WHERE idusuario= :ID", array(':LOGIN'=>$this->getDeslogin(),
-        ':PASSWORD'=>$this->getDessenha(),
-        ':ID'=>$this->getIdusuario()
-        ));
-	}
-
-    public function delete(){
-
-		$sql = new Sql();
-
-		$sql->execQuery("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
-			':ID'=>$this->getIdusuario()
-		));
-
-		$this->setIdusuario(0);
-		$this->setDeslogin("");
-		$this->setDessenha("");
-		$this->setDtcadastro(new DateTime());
-
-	}
-
-    public function __construct($login = "", $password = ""){
-
-		$this->setDeslogin($login);
-		$this->setDessenha($password);
-
-	}
    
    public function __toString(){
 
--- index.php
+++ index.php
@@ -8,8 +8,48 @@ require_once("config.php");
 //echo json_encode($usuarios);
 
 
-//Seleciona um usuario especifico
+/*Seleciona um usuario especifico
 $root = new usuario();
 $root->loadbyId(3);
-echo $root;
+echo $root;*/
+
+/*Carrega uma lista de Usuarios - O metodo é ESTATICO,
+chama direto o metodo, usando ::   
+$lista = usuario::getList();
+echo json_encode($lista);*/
+
+/*Carrega uma lista buscando pelo LOGIN   
+$search = usuario::search("ib");
+echo json_encode($search);*/
+
+/*Carrega um usuario usando o LOGIN e a SENHA
+$usuario = new usuario();
+$usuario->login("ibk", "567");
+echo $usuario;*/
+
+
+
+/*Incluir REGISTROS
+$aluno = new usuario("aluno4", "123");
+$aluno->insert();
+echo $aluno;*/
+
+/*Alterando um USUARIO especifico
+$usuario = new usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "3");
+
+echo $usuario;*/
+
+/*Alterando um USUARIO especifico*/
+$usuario = new usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
+
 ?>
